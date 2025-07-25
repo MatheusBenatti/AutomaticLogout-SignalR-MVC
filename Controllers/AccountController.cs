03ac629c547181834930f75f3b1a6362b1580922
theus <theus@BENATTIX>
2025-07-25 15:15:32 -0300

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static System.Collections.Specialized.BitVector32;
+
+namespace websocket.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            if (username == "admin" && password == "123")
+            {
+                Session["User"] = username;
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Usuário ou senha inválidos";
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+
+}
---
+++ Hubs/UserHub.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.SignalR;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace websocket.Hubs
+{
+    public class UserHub : Hub
+    {
+        private static ConcurrentDictionary<string, int> _userConnections = new ConcurrentDictionary<string, int>();
+
+        public override Task OnConnected()
+        {
+            var username = Context.QueryString["username"];
+            if (!string.IsNullOrEmpty(username))
+            {
+                _userConnections.AddOrUpdate(username, 1, (key, old) => old + 1);
+            }
+
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var username = Context.QueryString["username"];
+            if (!string.IsNullOrEmpty(username) && _userConnections.ContainsKey(username))
+            {
+                _userConnections.AddOrUpdate(username, 0, (key, old) => old - 1);
+
+                if (_userConnections[username] <= 0)
+                {
+                    _userConnections.TryRemove(username, out _);
+                }
+            }
+
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
